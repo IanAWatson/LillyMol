3322b2f7cb0748935eaa3dfa0dbdcd250151efd1
Ian Watson <ianiwatson@gmail.com>
2026-02-14 07:30:05 -0500

    spreadplot updates

--- contrib/bin/spreadplot.jl
+++ contrib/bin/spreadplot.jl
@@ -4,13 +4,42 @@ using ArgMacros
 
 using CSV
 using Plots
+using Dierckx
 
+# This supports the --dist option.
 function crossing_point(values::Array, cutoff::Real)::Int
   for i in eachindex(values)
     values[i] <= cutoff && return i
   end
 
   return 1;
+end
+
+# This supports the --points option
+# Assumes that `values` is an output from spread.
+# Return the indices at which `values` crosses the values implied by `delta`.
+# Delta is a distance, like 0.1. In that case we return the indices at which
+# `values` crosses [0.9, 0.8, 0.7...]
+function crossing_points(values::Array, delta::Real)::Array{Int}
+  result_size = convert(Int, round(1.0 / delta)) + 1
+  result = zeros(Int32, result_size)
+
+  dists = collect(range(1.0, stop=0.0, length=convert(Int, round(1.0 / delta) + 1)))
+  # An index into the `dists` array. The last value will be 0, so skip that.
+  ndx = length(dists) - 1
+  # Scan in reverse order
+  for i in length(values):-1:1
+    v = values[i]
+    v == 0.0 && continue
+    if v > dists[ndx]
+      result[ndx] = i
+      ndx -= 1
+    end
+  end
+
+  # get rid of zero's
+  return filter(e -> e > 0, result)
+
 end
 
 function main()
@@ -23,12 +52,16 @@ function main()
     --lwd : controls the width of the line, --lwd 8
     --color : the color of the line, --color red
     --dist : draw a horizontal line at the point where a distance is crossed, --dist 0.15
+    --spline : use spline interpolation. Useful for large datasets and svg output, --spline <npoints>
+    --point : draw dots on the curve every specified interval, --point 0.1
     The 
     """
     @argumentrequired String stem "-S"
     @argumentoptional Float32 threshold "--dist"
     @argumentoptional String label "--label"
     @argumentoptional String title "--title"
+    @argumentoptional Int32 spline "--spline"
+    @argumentoptional Float32 point "--point"
     @argumentdefault String "png" format "--format"
     @argumentdefault Int32 4 lwd "--lwd"
     @argumentdefault String "green" colour "--color"
@@ -41,12 +74,26 @@ function main()
   if isnothing(label)
     label = "Selected"
   end
-
-  plot(range(1,length(distance)), distance, ylim=(0.0, distance[2]), xlabel="Number Selected",
-       ylabel="Distance", lwd=lwd, color=Symbol(colour), label=label)
+  
+  if isnothing(spline)
+    plot(range(1,length(distance)), distance, ylim=(0.0, distance[2]), xlabel="Number Selected",
+         ylabel="Distance", lwd=lwd, color=Symbol(colour), label=label)
+  else
+    x = range(1, stop=length(distance), length=spline)
+    spl = Spline1D(range(1,length(distance)), distance)
+    tmp = [spl(q) for q in x]
+    plot(x, tmp, ylim=(0.0, distance[2]), xlabel="Number Selected",
+         ylabel="Distance", lwd=lwd, color=Symbol(colour), label=label)
+  end
 
    if ! isnothing(title)
      plot!(title=title)
+   end
+
+   if ! isnothing(point)
+     x = crossing_points(distance, point)
+     y = [distance[i] for i in x]
+     scatter!(x, y, label=false)
    end
 
   if ! isnothing(threshold)

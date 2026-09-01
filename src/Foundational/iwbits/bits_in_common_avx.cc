9967a191f0834a5476d1e78e9b1efc2faae350a0
Ian Watson <ianiwatson@gmail.com>
2026-09-01 06:58:31 -0400

    bits_in_common_avx.cc fixes

--- src/Foundational/iwbits/bits_in_common_avx.cc
+++ src/Foundational/iwbits/bits_in_common_avx.cc
@@ -20,7 +20,7 @@ intersection_popcount_avx512(const uint64_t* a,
                              const uint64_t* b,
                              std::size_t n_words) {
     std::size_t i = 0;
-    uint64_t total = 0;
+    __m512i vtotal = 0;
 
     alignas(64) uint64_t lanes[8];
 
@@ -28,15 +28,9 @@ intersection_popcount_avx512(const uint64_t* a,
         __m512i va = _mm512_loadu_si512(reinterpret_cast<const __m512i*>(a + i));
         __m512i vb = _mm512_loadu_si512(reinterpret_cast<const __m512i*>(b + i));
         __m512i vand = _mm512_and_si512(va, vb);
-
-        // Popcount each 64-bit lane
-        __m512i vcnt = _mm512_popcnt_epi64(vand);
-
-        // Store and horizontally sum 8 lanes
-        _mm512_store_si512(reinterpret_cast<__m512i*>(lanes), vcnt);
-        total += lanes[0] + lanes[1] + lanes[2] + lanes[3]
-              +  lanes[4] + lanes[5] + lanes[6] + lanes[7];
+        vtotal = _mm512_add_epi64(vtotal, _mm512_popcnt_epi64(vand);
     }
+    uint64_t total = _mm512_reduce_add_epi64(vtotal);
 
     // Tail
     for (; i < n_words; ++i) {
